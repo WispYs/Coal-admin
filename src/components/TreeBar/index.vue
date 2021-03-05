b8eca05f02279731553eae5bba70bc2e72044034
wisper <wisper927@163.com>
2021-03-05 18:01:11 +0800

    feat: add treebar component

--- src/components/TreeBar/index.vue
+++ src/components/TreeBar/index.vue
@@ -2,6 +2,7 @@
   <div class="tree-container">
     <div class="tree-title">{{ treeData.title }}</div>
     <el-tree :data="treeData.list" :props="defaultProps" @node-click="handleNodeClick" />
+    <div class="extend-button" @click="handleExtend" />
   </div>
 </template>
 <script>
@@ -14,6 +15,7 @@ export default {
   },
   data() {
     return {
+      treeOpen: true,
       defaultProps: {
         children: 'children',
         label: 'label'
@@ -24,6 +26,10 @@ export default {
     // 树形控件
     handleNodeClick(data) {
       console.log(data)
+    },
+
+    handleExtend() {
+      this.$emit('extend-click')
     }
   }
 }
