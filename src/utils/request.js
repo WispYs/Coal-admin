8f1e2f593bfcd05e7950627c7bd68af258e73dd4
wisper <wisper927@163.com>
2021-03-09 16:45:58 +0800

    feat: modify request field in headers 'Token' -> 'Authorization'

--- src/utils/request.js
+++ src/utils/request.js
@@ -13,7 +13,7 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     if (store.getters.token) {
-      config.headers['X-Token'] = getToken()
+      config.headers['Authorization'] = getToken()
     }
     return config
   },
